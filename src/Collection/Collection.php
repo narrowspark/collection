3650a7e2547bfce41c798a2c5e800304056b6f96
Daniel Bannert <d.bannert@anolilab.de>
2016-09-13 22:44:49 +0200

    start working

---
+++ src/Collection/Collection.php
@@ -0,0 +1,291 @@
+<?php
+declare(strict_types=1);
+namespace Narrowspark\Collection;
+
+use ArrayAccess;
+use ArrayIterator;
+use BadMethodCallException;
+use CachingIterator;
+use Countable;
+use Closure;
+use JsonSerializable;
+use IteratorAggregate;
+use Serializable;
+use RecursiveArrayIterator;
+use Traversable;
+use Iterator;
+
+class Collection implements ArrayAccess, Countable, IteratorAggregate, JsonSerializable, Serializable
+{
+    /**
+     * The registered string macros.
+     *
+     * @var array
+     */
+    protected static $macros = [];
+
+    /**
+     * The items contained in the collection.
+     *
+     * @var array
+     */
+    protected $items = [];
+
+    /**
+     * Create a new collection.
+     *
+     * @param callable|Closure|array|Traversable\Iterator|self|IteratorAggregate|JsonSerializable $items
+     */
+    public function __construct($items = [])
+    {
+        $this->items = $this->getArrayableItems($items);
+    }
+
+    /**
+     * Get all of the items in the collection.
+     *
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return array_map(function ($value) {
+            if (method_exists($value, 'toArray')) {
+                return $value->toArray();
+            }
+
+            return $value;
+        }, $this->items);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize()
+    {
+        return array_map(function ($value) {
+            if ($value instanceof JsonSerializable) {
+                return $value->jsonSerialize();
+            } elseif (method_exists($value, 'toJson')) {
+                return json_decode($value->toJson(), true);
+            } elseif (method_exists($value, 'toArray')) {
+                return $value->toArray();
+            } else {
+                return $value;
+            }
+        }, $this->items);
+    }
+
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param int $options
+     *
+     * @return string
+     */
+    public function toJson(int $options = 0): string
+    {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * Get an iterator for the items.
+     *
+     * @return \ArrayIterator
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    /**
+     * Get a CachingIterator instance.
+     *
+     * @param int $flags
+     *
+     * @return \CachingIterator
+     */
+    public function getCachingIterator(int $flags = CachingIterator::CALL_TOSTRING): CachingIterator
+    {
+        return new CachingIterator($this->getIterator(), $flags);
+    }
+
+    /**
+     * Count the number of items in the collection.
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    /**
+     * Convert the collection to its string representation.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->toJson();
+    }
+
+    /**
+     * Determine if an item exists at an offset.
+     *
+     * @param mixed $key
+     *
+     * @return bool
+     */
+    public function offsetExists($key)
+    {
+        return array_key_exists($key, $this->items);
+    }
+
+    /**
+     * Get an item at a given offset.
+     *
+     * @param mixed $key
+     *
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->items[$key];
+    }
+
+    /**
+     * Set the item at a given offset.
+     *
+     * @param mixed $key
+     * @param mixed $value
+     */
+    public function offsetSet($key, $value)
+    {
+        if (is_null($key)) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$key] = $value;
+        }
+    }
+
+    /**
+     * Unset the item at a given offset.
+     *
+     * @param string $key
+     */
+    public function offsetUnset($key)
+    {
+        unset($this->items[$key]);
+    }
+
+    /**
+     * Register a custom macro.
+     *
+     * @param string   $name
+     * @param callable $macro
+     */
+    public static function macro(string $name, callable $macro)
+    {
+        static::$macros[$name] = $macro;
+    }
+
+    /**
+     * Checks if macro is registered.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public static function hasMacro(string $name): bool
+    {
+        return isset(static::$macros[$name]);
+    }
+
+    /**
+     * Dynamically handle calls to the class.
+     *
+     * @param string $method
+     * @param array  $parameters
+     *
+     * @return mixed
+     *
+     * @throws \BadMethodCallException
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        if (! static::hasMacro($method)) {
+            throw new BadMethodCallException("Method {$method} does not exist.");
+        }
+
+        if (static::$macros[$method] instanceof Closure) {
+            return call_user_func_array(Closure::bind(static::$macros[$method], null, static::class), $parameters);
+        }
+
+        return call_user_func_array(static::$macros[$method], $parameters);
+    }
+
+    /**
+     * Dynamically handle calls to the class.
+     *
+     * @param string $method
+     * @param array  $parameters
+     *
+     * @return mixed
+     *
+     * @throws \BadMethodCallException
+     */
+    public function __call($method, $parameters)
+    {
+        if (! static::hasMacro($method)) {
+            throw new BadMethodCallException("Method {$method} does not exist.");
+        }
+
+        if (static::$macros[$method] instanceof Closure) {
+            return call_user_func_array(static::$macros[$method]->bindTo($this, static::class), $parameters);
+        }
+
+        return call_user_func_array(static::$macros[$method], $parameters);
+    }
+
+     /**
+     * Results array of items from Collection or Arrayable.
+     *
+     * @param callable|Closure|array|Traversable\Iterator|self|IteratorAggregate|JsonSerializable $items
+     *
+     * @return array
+     */
+    protected function getArrayableItems($items): array
+    {
+        if (is_array($items)) {
+            return new RecursiveArrayIterator($items);
+        } elseif (is_callable($items)) {
+            return $items();
+        } elseif ($items instanceof self) {
+            return $items->all();
+        } elseif ($items instanceof IteratorAggregate) {
+            return $items->getIterator();
+        } elseif ($items instanceof Iterator) {
+            return $items;
+        } elseif ($items instanceof Traversable) {
+            return iterator_to_array($items);
+        } elseif ($items instanceof JsonSerializable) {
+            return $items->jsonSerialize();
+        } elseif (method_exists($items, 'toJson')) {
+            return json_decode($items->toJson(), true);
+        } elseif (method_exists($items, 'toArray')) {
+            return $items->toArray();
+        }
+
+        return (array) $items;
+    }
+}
